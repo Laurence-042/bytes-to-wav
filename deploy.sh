5dd1490b1250939a4ef7176f94ea44ea494d5bf0
Laurence042 <qq2120286535@gmail.com>
2020-10-24 13:50:30 +0800

    modify deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Laurence-042/color-change-image.git master:gh-pages
+git push -f git@github.com:Laurence-042/bytes-to-wav.git master:gh-pages
 
 cd -
